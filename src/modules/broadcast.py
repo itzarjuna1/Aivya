f084ca243ed94d09e67093d4cf11af07cef634de
Cʜɪᴋᴜ 🐾 <167273277+MaybeChiku@users.noreply.github.com>
2025-12-10 06:54:28 +0530

    Ntg

--- src/modules/broadcast.py
+++ src/modules/broadcast.py
@@ -6,9 +6,9 @@ from pyrogram.types import Message
 
 from src import app
 from src.database import get_chats
-from config import OWNER_ID
 
-@app.on_message(filters.command("broadcast") & filters.user(OWNER_ID))
+
+@app.on_message(filters.command("logs") & filters.user(app.owner))
 async def broadcast_(_, message: Message):
     """Broadcasts a single message to all chats and users."""
 
